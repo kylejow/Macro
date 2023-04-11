3163a8f3673b440c71cd4757d6f82a2c2f8b1011
kylejow <77653395+kylejow@users.noreply.github.com>
2023-04-11 01:45:23 -0700

    Record positions

--- position.py
+++ position.py
@@ -4,6 +4,7 @@ import pyautogui
 import keyboard
 import threading
 import os
+from win32api import GetKeyState
 
 stop = False
 def interrupt():
@@ -14,11 +15,19 @@ run = threading.Thread(target=interrupt)
 run.start()
 os.system('clear')
 
-print("Esc to exit\n")
+print("Click to save positions\nEsc to exit\n")
+points = []
+
 while(not stop):
     x, y = pyautogui.position()
-    positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+    if((GetKeyState(0x01) & 0x80) != 0):
+        points.append([x, y])
+        while((GetKeyState(0x01) & 0x80)):
+            continue
+    positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4) + '        '
     print(positionStr, end = '\r')
 
 os.system('clear')
 run.join()
+for i, point in enumerate(points, 1):
+    print(str(i) + '. ' + str(point[0]) + ', ' + str(point[1]))
