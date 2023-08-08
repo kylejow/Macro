2431afb503fa107fb4fdaf8e235bdf9fa9708684
Kyle Jow <77653395+kylejow@users.noreply.github.com>
2023-08-08 10:30:27 -0700

    add keyboard input

--- input.py
+++ input.py
@@ -6,7 +6,6 @@ class input:
     def __init__(self, duration):
         self.duration = duration
 
-
 class mouseInput(input):
     def __init__(self, duration, point):
         super().__init__(duration)
@@ -17,8 +16,15 @@ class mouseInput(input):
         pyautogui.mouseUp()
 
 
-
 class keyboardInput(input):
     def __init__(self, duration, key):
         super().__init__(duration)
         self.key = key
+    def send(self):
+        pyautogui.keyDown(self.key)
+        Sleep(self.duration)
+        pyautogui.keyUp(self.key)
+    def sendDown(self):
+        pyautogui.keyDown(self.key)
+    def sendUp(self):
+        pyautogui.keyUp(self.key)
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #https://github.com/asweigart/pyautogui
 from position import getPoints
+import input
+from win32api import Sleep
 
 points = getPoints()
 
 for i, point in enumerate(points, 1):
     print(str(i) + '. ' + str(point.x) + ', ' + str(point.y))
+
+Sleep(3000)
+testInputs = []
+testInputs.append(input.mouseInput(10, points[0]))
+testInputs.append(input.keyboardInput(10, "a"))
+for input in testInputs:
+    input.send()
